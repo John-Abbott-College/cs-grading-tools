1d682ad4632816488064695640baa89701b91ca0
michaelhaaf <michael.haaf@gmail.com>
2025-12-13 23:48:54 -0500

    a3 individual grading 'works'

--- src/a3/run_tests.py
+++ src/a3/run_tests.py
@@ -1,7 +1,26 @@
 import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
+import socket
+import os
+import sys
 
-if __name__ == '__main__':
-    suite = unittest.defaultTestLoader.discover('tests')
-    with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(visibility='visible', stream=f).run(suite)
+"""
+Usage:
+python run_tests.py <student_submission_folder>
+
+Works on gradescope too.
+"""
+if __name__ == "__main__":
+    if socket.gethostname() == "home":
+        if len(sys.argv) != 2:
+            print("student submission directory needed. Exiting.")
+            sys.exit(1)
+        prefix = sys.argv[1]
+    else:
+        prefix = "/autograder"
+
+    suite = unittest.defaultTestLoader.discover("tests")
+    results_file = f"{prefix}/results.json"
+
+    with open(results_file, "w") as f:
+        JSONTestRunner(visibility="visible", stream=f).run(suite)
--- src/a3/tests/test_helper.py
+++ src/a3/tests/test_helper.py
@@ -6,7 +6,8 @@ from modulefinder import Module
 from types import ModuleType
 from typing import Callable
 
-def get_inner_func(student_code: ModuleType) -> dict[str,list[str]]:
+
+def get_inner_func(student_code: ModuleType) -> dict[str, list[str]]:
     source = inspect.getsource(student_code)
     nodes = ast.walk(ast.parse(source))
 
@@ -14,30 +15,39 @@ def get_inner_func(student_code: ModuleType) -> dict[str,list[str]]:
 
     inner_func_dict = {}
     for outer_func in func_nodes:
-        inner_func_dict[outer_func.name] = \
-            [inner_func.name for inner_func in ast.walk(outer_func)
-             if isinstance(inner_func, ast.FunctionDef) and inner_func.name != outer_func.name]
-
+        inner_func_dict[outer_func.name] = [
+            inner_func.name
+            for inner_func in ast.walk(outer_func)
+            if isinstance(inner_func, ast.FunctionDef)
+            and inner_func.name != outer_func.name
+        ]
 
     return inner_func_dict
 
-def uses_loop(function:Callable):
+
+def uses_loop(function: Callable):
     nodes = ast.walk(ast.parse(inspect.getsource(function)))
     return any(isinstance(node, (ast.For, ast.While)) for node in nodes)
 
-def uses_condition(function:Callable):
-    nodes = ast.walk(ast.parse(inspect.getsource(function)))
-    return any(isinstance(node,ast.If) for node in nodes)
 
-def get_signature(function:Callable)->Signature:
+def uses_condition(function: Callable):
+    nodes = ast.walk(ast.parse(inspect.getsource(function)))
+    return any(isinstance(node, ast.If) for node in nodes)
+
+
+def get_signature(function: Callable) -> Signature:
     return inspect.signature(function)
 
 
-def get_func_calls(caller:Callable)->list[str]:
+def get_func_calls(caller: Callable) -> list[str]:
     nodes = ast.walk(ast.parse(inspect.getsource(caller)))
-    func_calls = (call for call in nodes if isinstance(call,ast.Call))
-    call_names = [call.func.id for call in func_calls if isinstance(call.func,ast.Name)]
+    func_calls = (call for call in nodes if isinstance(call, ast.Call))
+    call_names = [
+        call.func.id for call in func_calls if isinstance(call.func, ast.Name)
+    ]
     return call_names
 
-def contains_func(student_code :Module, func_name:str )->bool:
+
+def contains_func(student_code: Module, func_name: str) -> bool:
     return hasattr(student_code, func_name)
+
